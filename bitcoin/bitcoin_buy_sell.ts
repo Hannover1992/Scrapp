a2ef82559bf644ef5b7bc09e62df7fcc54db0b0c
PatrykKrzyzanski <Hannover1992@googlemail.com>
2022-12-23 10:54:44 +0100

    create urls for precious metalls

--- bitcoin/bitcoin_buy_sell.ts
+++ bitcoin/bitcoin_buy_sell.ts
@@ -1,12 +1,37 @@
 import {Browser, Page} from "puppeteer";
 import puppeteer from "puppeteer";
 
+async function get_the_div_with_price(browser: Browser, etoroUrl: string) {
+    const page = await browser.newPage();
+    await navigateToUrl(etoroUrl, page);
+    // Find all divs with class price
+    const priceDivs = await page.$$('div.etoro-price-value');
+    return {page, priceDivs};
+}
+
+// concatinate_e_toro_url_and_stocks(etorourl, precious_metal)
+function concatinate_e_toro_url_and_stocks(stock: string[]) {
+    let etoroUrl = 'https://www.etoro.com/markets/';
+    const chart = '/chart';
+    return stock.map(metal => etoroUrl + metal + chart);
+}
+
+function get_URL() {
+    function precius_metal_shorts() {
+        const gold = ['phys', 'gdxj'];
+        const silver = ['pslv', 'sbsw'];
+        const platinum = ['pall', 'pall'];
+        const precious_metal = gold.concat(silver, platinum);
+        return precious_metal;
+    }
+
+    let precious_metal = precius_metal_shorts();
+    let precious_metal_URL = concatinate_e_toro_url_and_stocks(precious_metal);
+    return precious_metal_URL;
+}
+
 (async () => {
-    let etoroUrlArr = [
-        'https://www.etoro.com/markets/btc/chart',
-        'https://www.etoro.com/markets/eth/chart',
-        'https://www.etoro.com/markets/xrp/chart',
-    ];
+    let etoroUrlArr = get_URL();
 
     // Add stealth plugin and use defaults (all tricks to hide puppeteer usage)
     const browser = await setUpWebBrowser();
@@ -14,12 +39,7 @@ import puppeteer from "puppeteer";
     // Use Promise.all to make the loop async
     await Promise.all(
         etoroUrlArr.map(async (etoroUrl) => {
-            const page = await browser.newPage();
-            await navigateToUrl(etoroUrl, page);
-
-            // Find all divs with class price
-            const priceDivs = await page.$$('div.etoro-price-value');
-
+            const {page, priceDivs} = await get_the_div_with_price(browser, etoroUrl);
             // Extract the text content of the span elements
             while(true) {
                 const prices = await Promise.all(
@@ -28,14 +48,12 @@ import puppeteer from "puppeteer";
                             const span = element.querySelector('span[automation-id="buy-sell-button-rate-value"]');
                             return span ? span.textContent : null;
                         }, div);
-
                         return spanText;
                     }),
                 );
 
                 //console log the url and the price
                 console.log(etoroUrl, prices);
-
                 await page.waitForTimeout(1000);
             }
         }),
